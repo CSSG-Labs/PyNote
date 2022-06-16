bbf6e91591eb98c415c84978b8ffffb9f720e4d6
Patrick <pkbruso@gmail.com>
2022-06-16 10:18:43 -0700

    Created new app with pyside6 to generate window/text-box

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-import tkinter as tk
-
-root = tk.Tk("PyNote")
-text = tk.Text(root)
-text.grid()
-root.mainloop()
+import sys
+from PySide6.QtWidgets import QMainWindow, QApplication, QLineEdit
+ 
+class App(QMainWindow):
+ 
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyNote'
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 240
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(self.width, self.height)
+    
+        # Create textbox
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 20)
+        self.textbox.resize(280,200)
+        
+        self.show()
+ 
+ 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec())
