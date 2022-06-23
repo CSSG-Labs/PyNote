4a879531816c51bf09608110939a34dc9d089f96
Patrick Bruso <pkbruso@gmail.com>
2022-06-23 11:40:24 -0700

    Added simple code to generate window with text box

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-import sys
-from PySide6.QtWidgets import QMainWindow, QApplication, QPlainTextEdit
- 
-class App(QMainWindow):
- 
-    def __init__(self):
-        super().__init__()
-        self.title = 'PyNote'
-        self.left = 10
-        self.top = 10
-        self.width = 320
-        self.height = 240
-        self.initUI()
-    
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.resize(self.width, self.height)
-    
-        # Create textbox
-        self.textbox = QPlainTextEdit(self)
-        self.textbox.move(20, 20)
-        self.textbox.resize(280,200)
-        
-        self.show()
- 
- 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec())
+import PySimpleGUI as sg
+
+#set the theme for the screen/window
+sg.theme('SystemDefault')
+
+#define layout
+layout=[[sg.Multiline(size=(80,10),tooltip='Write your Text here')]]
+
+#Define Window
+win =sg.Window('PyNote',layout)
+
+#Read values entered by user
+events,values = win.read()
+
+#close first window
+win.close()
