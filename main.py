2cb89853e4e1e52a06a330ef6fd945379273bfae
Patrick <pkbruso@gmail.com>
2022-08-10 12:01:06 -0700

    Added saveas function to save text

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import sys
 import tkinter as tk
+import tkFileDialog
 
 root =tk.Tk()
+
+# Create text box for window
 text = tk.Text(root)
 text.grid()
+
+# Save function
+def saveas():
+    global text
+    t = text.get("1.0", "end-1c")
+    save_location = tkFileDialog.asksaveasfilename()
+    file1 = open(save_location, "w+")
+    file1.write(t)
+    file1.close()
+
+# Start program
 root.mainloop()
