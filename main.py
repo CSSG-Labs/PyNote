bd8c2e842b1ad9129df732df6fb859f0796b582f
SamReiCooper <konstantin.marchuk@protonmail.com>
2022-08-11 12:10:18 +0200

    Added Application class

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import tkinter as tk
 from tkinter import filedialog
 
-root =tk.Tk()
-root.title("PyNote")
-
-# Create text box for window
-text = tk.Text(root)
-text.grid()
-
-# Save function
-def saveas():
-    global text
-    t = text.get("1.0", "end-1c")
-    save_location = filedialog.asksaveasfilename()
-    file1 = open(save_location, "w+")
-    file1.write(t)
-    file1.close()
-
-# Create save button
-button = tk.Button(root, text="Save", command=saveas)
-button.grid()
+class Application():
+    def __init__(self):
+        # Create text box for window
+        self.text = tk.Text(root)
+        self.text.grid()
+        # Create save button
+        self.button = tk.Button(root, text="Save", command=self.saveas)
+        self.button.grid()
+    
+    # Save function
+    def saveas(self):
+        t = self.text.get("1.0", "end-1c")
+        save_location = filedialog.asksaveasfilename()
+        file1 = open(save_location, "w+")
+        file1.write(t)
+        file1.close()
 
 # Start program
+root =tk.Tk()
+root.title("PyNote")
+app = Application()
 root.mainloop()
