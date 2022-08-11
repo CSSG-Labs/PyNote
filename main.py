1d3705e96551deb6dc42ef4389995ca9e9a41be3
SamReiCooper <konstantin.marchuk@protonmail.com>
2022-08-11 16:13:13 +0200

    Application class creation and improvement

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import tkinter as tk
 from tkinter import filedialog
 
-class Application():
+class Application(tk.Frame):
     def __init__(self):
+        tk.Frame.__init__(self)
+
         # Create text box for window
-        self.text = tk.Text(root)
-        self.text.grid()
+        self.master.title("PyNote")
+        self.text = tk.Text(self)
+        self.text.pack(side="top")
+
         # Create save button
-        self.button = tk.Button(root, text="Save", command=self.saveas)
-        self.button.grid()
-    
+        self.button = tk.Button(self, text="Save", command=self.saveas)
+        self.button.pack(side="bottom")
+        self.pack()
+
     # Save function
     def saveas(self):
         t = self.text.get("1.0", "end-1c")
@@ -19,7 +24,5 @@ class Application():
         file1.close()
 
 # Start program
-root =tk.Tk()
-root.title("PyNote")
 app = Application()
-root.mainloop()
+app.mainloop()
