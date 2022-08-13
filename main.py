74ab05d0e03939c876469cb419898344457790b5
Patrick <pkbruso@gmail.com>
2022-08-13 14:47:14 -0700

    Added open function to open file and read content to text box
    Defined "open" function that will call filedialog for the user to select a text file, and then read that text file into the text box.  It will also save the file location to the global file_location variable.  The open function is connected to the "Open" label in the file menu.

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ class Application(tk.Frame):
 
         self.master.title("PyNote")
 
+        # Save file location for opened files
+        self.file_location = None
+
         # Create menu bar
         self.menuBar = tk.Menu(self.master)
         fileMenu = tk.Menu(self.menuBar, tearoff=0)
         self.master.config(menu=self.menuBar)
         fileMenu.add_command(label="New", command=None)
-        fileMenu.add_command(label="Open", command=None)
+        fileMenu.add_command(label="Open", command=self.open)
         fileMenu.add_command(label="Save", command=None)
         fileMenu.add_command(label="Save as...", command=self.saveas)
         self.menuBar.add_cascade(label="File", menu=fileMenu)
@@ -31,6 +34,20 @@ class Application(tk.Frame):
             file1 = open(saveLocation, "w+")
             file1.write(t)
             file1.close()
+
+    # Open function
+    def open(self):
+        # Placeholder for future CommandPrompt class call
+
+        # Find location of file to open
+        open_location = filedialog.askopenfilename(title='Open Text File', filetypes=[('text files', '*.txt')])
+        
+        if (open_location != ''): # Check file was selected and .txt file
+            opened_file = open(open_location, "r") # Open file
+            opened_text = opened_file.read() # Read file and save text
+            self.text.insert("1.0", opened_text) # Insert text to text box at line 1, character 0
+            opened_file.close() # Close file
+            self.file_location = open_location # Set file_location variable to opened file location
 
 # Start program
 root = tk.Tk()
